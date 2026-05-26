df59fa75107f44805a5f4dfce7320c4d1e742328
Antonio Hernández <62889375+sirtica@users.noreply.github.com>
2026-05-26 19:10:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ genai.configure(
     api_key=os.environ.get("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 
 @app.route("/")
